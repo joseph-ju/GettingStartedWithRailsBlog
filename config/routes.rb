9aa18aa8f893f0eb0154df147373ec7a99917fb2
joseph <103565007+joseph-ju@users.noreply.github.com>
2023-10-16 22:57:31 +0900

    Add routes of comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   root "articles#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
