33325b16e3582f9c33565b3bbedd3f35b28c2e7b
joseph <103565007+joseph-ju@users.noreply.github.com>
2023-10-16 21:55:03 +0900

    Add resources of articles

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   root "articles#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-
+  resources :articles
   # Defines the root path route ("/")
   # root "posts#index"
 end
